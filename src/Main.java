5e2a7111d4bd928b8b7cb29f9b638546333940e6
bilal karahan <bilalkarahan309@gmail.com>
2022-10-03 14:35:39 +0300

    2

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Fighter marc=new Fighter("Marc",13,90,79,36);
+        Fighter alex=new Fighter("Alex",19,63,111,46);
+        Ring r = new Ring(marc,alex , 75 , 115,50.0);
+        r.run();
     }
 }
